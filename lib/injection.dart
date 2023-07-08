aaccbb3707170ba8f34a142fbe5559c544382f0c
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-07-08 17:12:49 +0300

    Add UserService to setupLocator

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:librarium/service/authentication_service/authentication_service.dart';
+import 'package:librarium/service/user_service/user_service.dart';
 
 GetIt locator = GetIt.instance;
 
 void setupLocator() {
   locator.registerLazySingleton<AuthenticationService>(() => AuthenticationService());
+  locator.registerLazySingleton<UserService>(() => UserService());
 }
