c0737ead5e6c45035743bdbf829d15c5b118d942
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-07-08 17:12:25 +0300

    Fix imports to AuthenticationService class

--- lib/service/authentication_service/authentication_service.dart
+++ lib/service/authentication_service/authentication_service.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
 
-import 'package:http/http.dart' as http;
-import 'package:librarium/model/login.dart';
 import 'package:logger/logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'package:http/http.dart' as http;
+
 import '../../environment/environment.dart';
+import '../../model/login.dart';
 import '../../model/user.dart';
 
 class AuthenticationService {
