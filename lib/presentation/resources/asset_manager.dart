5f6801af6977c32fdc3b4ceebebfd90462ca7506
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-07-08 14:26:20 +0300

    Add login logo path to AppAsset class

--- lib/presentation/resources/asset_manager.dart
+++ lib/presentation/resources/asset_manager.dart
@@ -2,5 +2,6 @@ const String assetPath = "assets/images";
 
 class AppAsset {
   static const String defaultPp = "$assetPath/default_pp.png";
+  static const String loginLogo = "$assetPath/login_logo.png";
 
 }
