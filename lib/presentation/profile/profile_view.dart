02c04933134317e7b2ebbc15c48cfa112bd9ec93
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-07-08 18:04:13 +0300

    Update profile_view and profile_view_model

--- lib/presentation/profile/profile_view.dart
+++ lib/presentation/profile/profile_view.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:librarium/presentation/profile/profile_view_model.dart';
 import 'package:stacked/stacked.dart';
 
+import '../common/button/profile_button_view.dart';
+import '../resources/color_manager.dart';
+import '../resources/string_manager.dart';
+
 class ProfileView extends StatefulWidget {
   const ProfileView({Key? key}) : super(key: key);
 
@@ -12,10 +16,115 @@ class ProfileView extends StatefulWidget {
 class _ProfileViewState extends State<ProfileView> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
     return ViewModelBuilder<ProfileViewModel>.reactive(
-      viewModelBuilder: () => ProfileViewModel(context),
+      viewModelBuilder: () {
+        ProfileViewModel viewModel = ProfileViewModel(context);
+        viewModel.start();
+        return viewModel;
+      },
       builder: (context, viewModel, child) => Scaffold(
+        backgroundColor: AppColor.bgColor,
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: AppColor.transparent,
+          elevation: 0,
+          actions: [
+            IconButton(
+              onPressed: () => viewModel.goSettingView(),
+              splashRadius: 20,
+              icon: const Icon(Icons.settings, color: AppColor.grey,),
+            )
+          ],
+        ),
+        body: SingleChildScrollView(
+          primary: false,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: CircleAvatar(
+                  radius: 150,
+                  backgroundColor: AppColor.transparent,
+                  backgroundImage: viewModel.user.avatar != null
+                    ? MemoryImage(viewModel.user.avatar!)
+                    : null,
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  "@${viewModel.user.username}",
+                  textAlign: TextAlign.center,
+                ),
+                subtitle: Text(
+                  "${viewModel.user.firstName} ${viewModel.user.lastName}",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ),
+              SizedBox(
+                width: size.width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ProfileButtonView(
+                      onPressed: () => viewModel.showQuotesDialog(),
+                      content: AppString.quotes,
+                      count: "0", //${viewModel.user.quotes?.length}
+                    ),
+                    ProfileButtonView(
+                      onPressed: () => viewModel.showAddedBooksDialog(),
+                      content: AppString.books,
+                      count: "${viewModel.user.addedBooks?.length}",
+                    ),
+                    ProfileButtonView(
+                      onPressed: () => viewModel.showFollowingsDialog(),
+                      content: AppString.followings,
+                      count: "${viewModel.user.followings?.length}",
+                    ),
+                    ProfileButtonView(
+                      onPressed: () => viewModel.showFollowersDialog(),
+                      content: AppString.followers,
+                      count: "${viewModel.user.followers?.length}",
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(),
+              ListTile(
+                trailing: IconButton(
+                  onPressed: () {
 
+                  },
+                  splashRadius: 20,
+                  icon: Icon(
+                    Icons.edit,
+                    color: AppColor.blue900,
+                  ),
+                ),
+                title: Text(
+                  AppString.aboutMe.toUpperCase(),
+                  style: TextStyle(
+                    color: AppColor.blue900,
+                    fontWeight: FontWeight.w800,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 32,
+                    decoration: TextDecoration.underline
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  title: Text(
+                    viewModel.user.aboutMe ?? "",
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/presentation/profile/profile_view_model.dart
+++ lib/presentation/profile/profile_view_model.dart
@@ -1,4 +1,13 @@
-import 'package:librarium/presentation/common/main_view_model.dart';
+import 'package:logger/logger.dart';
+
+import '../../injection.dart';
+import '../../model/user.dart';
+import '../../service/user_service/user_service.dart';
+import '../common/dialog/profile_dialog/addedBooksDialog.dart';
+import '../common/dialog/profile_dialog/followersDialog.dart';
+import '../common/dialog/profile_dialog/followingsDialog.dart';
+import '../common/dialog/profile_dialog/quotesDialog.dart';
+import '../common/main_view_model.dart';
 
 class ProfileViewModel extends MainViewModel {
   ProfileViewModel(super.context);
@@ -6,6 +15,42 @@ class ProfileViewModel extends MainViewModel {
   @override
   void start() {
     // TODO: implement start
+    findUserDetail();
+  }
+
+  Logger logger = Logger();
+
+  listenToChanges() {
+    notifyListeners();
+  }
+
+  final UserService _userService = locator<UserService>();
+
+  User user = User();
+
+  findUserDetail() async {
+    user = await _userService.findUserDetail();
+    notifyListeners();
+  }
+
+  goSettingView() {
+
+  }
+
+  showQuotesDialog() {
+    quotesDialog(context, this);
+  }
+
+  showAddedBooksDialog() {
+    addedBooksDialog(context, this);
+  }
+
+  showFollowingsDialog() {
+    followingsDialog(context, this);
+  }
+
+  showFollowersDialog() {
+    followersDialog(context, this);
   }
 
 }
