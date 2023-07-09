cc17f3c5dd3a7af9087ef1b06fc9703c22a61e96
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-07-09 20:08:28 +0300

    Add add_book_view and add_book_view_model

--- lib/presentation/add_book/add_book_view.dart
+++ lib/presentation/add_book/add_book_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:librarium/presentation/add_book/add_book_view_model.dart';
 import 'package:stacked/stacked.dart';
+
+import '../resources/string_manager.dart';
 
 class AddBookView extends StatefulWidget {
   const AddBookView({Key? key}) : super(key: key);
@@ -12,10 +15,211 @@ class AddBookView extends StatefulWidget {
 class _AddBookViewState extends State<AddBookView> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
     return ViewModelBuilder<AddBookViewModel>.reactive(
-      viewModelBuilder: () => AddBookViewModel(context),
+      viewModelBuilder: () {
+        AddBookViewModel viewModel = AddBookViewModel(context);
+        viewModel.start();
+        return viewModel;
+      },
       builder: (context, viewModel, child) => Scaffold(
-
+        appBar: AppBar(
+          title: const Text("Add Book"),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: size.height/5,
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () => viewModel.useGallery(),
+                      child: Card(
+                        child: Center(
+                          child: viewModel.coverImage != null
+                            ? Image.memory(viewModel.coverImageBytes!)
+                            : const Padding(
+                            padding: EdgeInsets.all(4.0),
+                            child: Text(AppString.addCoverImage),
+                          ),
+                        ),
+                      ),
+                    ),
+                    viewModel.coverImage != null
+                      ? Padding(
+                      padding: const EdgeInsets.only(left: 4.0, right: 16.0),
+                      child: Column(
+                        children: [
+                          OutlinedButton(
+                            onPressed: () => viewModel.zoomImage(),
+                            child: const Icon(Icons.search),
+                          ),
+                          OutlinedButton(
+                            onPressed: () => viewModel.removeImage(),
+                            child: const Icon(Icons.delete),
+                          ),
+                        ],
+                      ),
+                    )
+                    : const SizedBox(),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: OutlinedButton(
+                              onPressed: () => viewModel.showSelectAuthorDialog(),
+                              style: ElevatedButton.styleFrom(
+                                fixedSize: Size(size.width/2, size.height/16),
+                              ),
+                              child: Text(viewModel.selectedAuthor != null ? "${viewModel.selectedAuthor?.firstName} ${viewModel.selectedAuthor?.lastName}" : AppString.selectAuthor),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: ListView(
+                  primary: false,
+                  physics: const BouncingScrollPhysics(),
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextFormField(
+                              controller: viewModel.titleController,
+                              onChanged: (val) => viewModel.imChanging(),
+                              decoration: const InputDecoration(
+                                label: Text(AppString.title),
+                                prefixIcon: Icon(Icons.keyboard_arrow_right_sharp)
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextFormField(
+                                controller: viewModel.languageController,
+                                onChanged: (val) => viewModel.imChanging(),
+                                inputFormatters: <TextInputFormatter>[
+                                  FilteringTextInputFormatter.allow(RegExp(r'[a-zA-ZıIğĞüÜşŞöÖçÇ ]'))
+                                ],
+                                decoration: const InputDecoration(
+                                  label: Text(AppString.language),
+                                  prefixIcon: Icon(Icons.keyboard_arrow_right_sharp)
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextFormField(
+                                controller: viewModel.isbnController,
+                                onChanged: (val) => viewModel.imChanging(),
+                                inputFormatters: <TextInputFormatter>[
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                decoration: const InputDecoration(
+                                  label: Text(AppString.isbn),
+                                  prefixIcon: Icon(Icons.keyboard_arrow_right_sharp)
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextFormField(
+                                controller: viewModel.pageCountController,
+                                onChanged: (val) => viewModel.imChanging(),
+                                inputFormatters: <TextInputFormatter>[
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                decoration: const InputDecoration(
+                                  label: Text(AppString.pageCount),
+                                  prefixIcon: Icon(Icons.keyboard_arrow_right_sharp)
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextFormField(
+                                controller: viewModel.priceController,
+                                onChanged: (val) => viewModel.imChanging(),
+                                inputFormatters: <TextInputFormatter>[
+                                  FilteringTextInputFormatter.allow(RegExp(r'[0-9\.]'))
+                                ],
+                                decoration: const InputDecoration(
+                                  label: Text(AppString.price),
+                                  prefixIcon: Icon(Icons.keyboard_arrow_right_sharp)
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextFormField(
+                              controller: viewModel.descriptionController,
+                              onChanged: (val) => viewModel.imChanging(),
+                              maxLength: 500,
+                              maxLines: 10,
+                              minLines: 1,
+                              decoration: const InputDecoration(
+                                label: Text(AppString.description),
+                                prefixIcon: Icon(Icons.keyboard_arrow_right_sharp)
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: viewModel.checkAll()
+                  ? () => viewModel.saveBook()
+                  : null,
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(size.width/2, size.height/16),
+                ),
+                child: const Text(AppString.saveBook),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/presentation/add_book/add_book_view_model.dart
+++ lib/presentation/add_book/add_book_view_model.dart
@@ -1,4 +1,19 @@
-import 'package:librarium/presentation/common/main_view_model.dart';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:logger/logger.dart';
+
+import '../../injection.dart';
+import '../../model/author.dart';
+import '../../model/book.dart';
+import '../../service/author_service/author_service.dart';
+import '../../service/book_service/book_service.dart';
+import '../common/dialog/selectAuthorDialog.dart';
+import '../common/dialog/zoomBookCoverImageDialog.dart';
+import '../common/main_view_model.dart';
+import '../common/snack_bar/show_snack_bar.dart';
+import '../resources/string_manager.dart';
 
 class AddBookViewModel extends MainViewModel {
   AddBookViewModel(super.context);
@@ -6,7 +21,147 @@ class AddBookViewModel extends MainViewModel {
   @override
   void start() {
     // TODO: implement start
+    findAllAuthors();
   }
 
+  Logger logger = Logger();
+
+  imChanging() {
+    notifyListeners();
+  }
+
+  checkAll() {
+    if (coverImage == null) {
+      return false;
+    }
+    if (titleController.text.length < 2 || titleController.text.length > 100) {
+      return false;
+    }
+    if (languageController.text.length < 3 || languageController.text.length > 50) {
+      return false;
+    }
+    if (isbnController.text.length != 13) {
+      return false;
+    }
+    if (pageCountController.text.isEmpty || pageCountController.text.length > 6) {
+      return false;
+    }
+    if (priceController.text.isEmpty || priceController.text.length > 8) {
+      return false;
+    }
+    if (descriptionController.text.isEmpty) {
+      return false;
+    }
+    if (selectedAuthor == null) {
+      return false;
+    }
+    return true;
+  }
+
+  final BookService _bookService = locator<BookService>();
+  final AuthorService _authorService = locator<AuthorService>();
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  TextEditingController languageController = TextEditingController();
+  TextEditingController isbnController = TextEditingController();
+  TextEditingController pageCountController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+
+  final ImagePicker _picker = ImagePicker();
+
+  XFile? coverImage;
+  Uint8List? coverImageBytes;
+
+  useGallery() async {
+    coverImage = await _picker.pickImage(source: ImageSource.gallery);
+    if (coverImage != null) {
+      coverImageBytes = await coverImage!.readAsBytes();
+      notifyListeners();
+    }
+    logger.i("Gallery used");
+  }
+
+  zoomImage() {
+    zoomBookCoverImage(context, coverImageBytes!);
+  }
+
+  removeImage() {
+    coverImage = null;
+    notifyListeners();
+  }
+
+  List<Author> authorList = [];
+  List<Author> filteredAuthorList = [];
+
+  findAllAuthors() async {
+    authorList = await _authorService.findAllAuthors();
+    filteredAuthorList = authorList;
+    notifyListeners();
+  }
+
+  void filterAuthorList(String val) {
+    if (val.isEmpty) {
+      filteredAuthorList = authorList;
+      notifyListeners();
+    }
+    else {
+      filteredAuthorList = authorList
+          .where(
+              (element) =>
+          element.firstName!.toLowerCase().contains(val.toLowerCase()) ||
+              element.lastName!.toLowerCase().contains(val.toLowerCase())
+      ).toList();
+      notifyListeners();
+    }
+  }
+
+  Author? selectedAuthor;
+
+  showSelectAuthorDialog() {
+    selectAuthorDialog(context, this);
+  }
+
+  selectAuthor(Author? val) {
+    selectedAuthor = val!;
+    notifyListeners();
+    Navigator.pop(context);
+  }
+
+  Book book = Book();
+  bool savedBook = false;
+
+  saveBook() async {
+    book
+      ..title = titleController.text.trim()
+      ..description = descriptionController.text.trim()
+      ..language = languageController.text.trim()
+      ..isbn = isbnController.text.trim()
+      ..pageCount = int.tryParse(pageCountController.text.trim())
+      ..coverImage = coverImageBytes
+      ..author = selectedAuthor
+      ..price = double.tryParse(priceController.text.trim());
+    notifyListeners();
+    logger.w(book.toJson());
+    savedBook = await _bookService.saveBook(book);
+    if (savedBook) {
+      showSnackBar(context, AppString.saveBookSuccessful);
+      clearForm();
+    }
+    else {
+      showSnackBar(context, AppString.saveBookFailed);
+    }
+  }
+
+  clearForm() {
+    coverImage = null;
+    titleController.text = "";
+    languageController.text = "";
+    isbnController.text = "";
+    pageCountController.text = "";
+    priceController.text = "";
+    descriptionController.text = "";
+    notifyListeners();
+  }
 
 }
