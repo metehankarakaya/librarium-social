349f377bd267bde239599e846a4be29991330f38
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-07-10 23:16:03 +0300

    Added QuoteService to locator class

--- lib/injection.dart
+++ lib/injection.dart
@@ -2,6 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:librarium/service/authentication_service/authentication_service.dart';
 import 'package:librarium/service/author_service/author_service.dart';
 import 'package:librarium/service/book_service/book_service.dart';
+import 'package:librarium/service/quote_service/quote_service.dart';
 import 'package:librarium/service/user_service/user_service.dart';
 
 GetIt locator = GetIt.instance;
@@ -11,4 +12,5 @@ void setupLocator() {
   locator.registerLazySingleton<UserService>(() => UserService());
   locator.registerLazySingleton<BookService>(() => BookService());
   locator.registerLazySingleton<AuthorService>(() => AuthorService());
+  locator.registerLazySingleton<QuoteService>(() => QuoteService());
 }
