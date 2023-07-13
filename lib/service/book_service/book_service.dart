94de0ef53483829c0551d0ac63825359a70c5592
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-07-13 13:05:11 +0300

    Add findRandomBooks method

--- lib/service/book_service/book_service.dart
+++ lib/service/book_service/book_service.dart
@@ -15,6 +15,7 @@ class BookService extends MainService {
   Logger logger = Logger();
 
   static const String saveBookApi = "/private-app-api/save/book";
+  static const String findRandomBooksApi = "/private-app-api/find/random/books";
 
   Future<bool> saveBook(Book book) async {
     String api = "${Environment().apiUrl}$saveBookApi";
@@ -35,6 +36,29 @@ class BookService extends MainService {
     }
     else {
       return throw Exception("Service 'saveBook' failed with statusCode: ${response.statusCode}");
+    }
+  }
+
+  Future<List<Book>> findRandomBooks() async {
+    String api = "${Environment().apiUrl}$findRandomBooksApi";
+
+    final SharedPreferences prefs = await getPrefs();
+
+    final response = await http.get(
+      Uri.parse(api),
+      headers: <String, String>{
+        "Content-Type": "application/json",
+        "Accept": "application/json; charset=UTF-8",
+        HttpHeaders.authorizationHeader: prefs.getString("token").toString()
+      },
+    );
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      final List<Book> bookList = List<Book>.from(jsonData.map((x) => Book.fromJson(x)));
+      return bookList;
+    }
+    else {
+      return throw Exception("Service 'findRandomBooks' failed with statusCode: ${response.statusCode}");
     }
   }
 
