ff32b275dad8ad8c56bb4cc7b7616a6aadb6ef34
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-07-20 01:48:19 +0300

    Added InteractiveViewer widget to zoom book cover image

--- lib/presentation/common/dialog/zoom_book_cover_image_dialog.dart
+++ lib/presentation/common/dialog/zoom_book_cover_image_dialog.dart
@@ -3,25 +3,22 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 
 void zoomBookCoverImage(BuildContext context, Uint8List coverImage) {
-  Size size = MediaQuery.sizeOf(context);
   showDialog(
     context: context,
-    builder: (context) => SimpleDialog(
-      children: [
-        Container(
-          width: size.width,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25)
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.memory(
-              coverImage,
-              fit: BoxFit.cover,
+    builder: (context) => StatefulBuilder(
+      builder: (context, setState) {
+        return SimpleDialog(
+          children: [
+            InteractiveViewer(
+              maxScale: 5.0,
+              child: Image.memory(
+                coverImage,
+                fit: BoxFit.contain,
+              ),
             ),
-          ),
-        ),
-      ],
+          ],
+        );
+      },
     )
   );
 }
