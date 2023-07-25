4b013002b51cae345f0c9bc595816993f12dd6bd
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-07-25 15:00:40 +0300

    Add post_service

--- lib/injection.dart
+++ lib/injection.dart
@@ -2,6 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:librarium/service/authentication_service/authentication_service.dart';
 import 'package:librarium/service/author_service/author_service.dart';
 import 'package:librarium/service/book_service/book_service.dart';
+import 'package:librarium/service/post_service/post_service.dart';
 import 'package:librarium/service/quote_service/quote_service.dart';
 import 'package:librarium/service/user_service/user_service.dart';
 
@@ -13,4 +14,5 @@ void setupLocator() {
   locator.registerLazySingleton<BookService>(() => BookService());
   locator.registerLazySingleton<AuthorService>(() => AuthorService());
   locator.registerLazySingleton<QuoteService>(() => QuoteService());
+  locator.registerLazySingleton<PostService>(() => PostService());
 }
---
+++ lib/service/post_service/post_service.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:logger/logger.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:http/http.dart' as http;
+
+import '../../environment/environment.dart';
+import '../../model/post.dart';
+import '../main/main_service.dart';
+
+class PostService extends MainService {
+
+  Logger logger = Logger();
+
+  static const String addPostApi = "/private-app-api/add/post";
+
+  Future<bool> addPost(Post post) async {
+    String api = "${Environment().apiUrl}$addPostApi";
+
+    final SharedPreferences prefs = await getPrefs();
+
+    final response = await http.post(
+      Uri.parse(api),
+      headers: <String, String>{
+        "Content-Type": "application/json",
+        "Accept": "application/json; charset=UTF-8",
+        HttpHeaders.authorizationHeader: prefs.getString("token").toString()
+      },
+      body: jsonEncode(post.toJson())
+    );
+    if (response.statusCode == 200) {
+      return bool.tryParse(response.body) ?? false;
+    }
+    else {
+      return throw Exception("Service 'addPost' failed with statusCode: ${response.statusCode}");
+    }
+  }
+
+}
