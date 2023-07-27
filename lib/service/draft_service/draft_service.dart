942438381b96f346c1acfeb8de2393493b084f80
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-07-28 02:48:44 +0300

    Add findMyDraft service method

--- lib/service/draft_service/draft_service.dart
+++ lib/service/draft_service/draft_service.dart
@@ -1,5 +1,40 @@
-import 'package:librarium/service/main/main_service.dart';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:logger/logger.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:http/http.dart' as http;
+
+import '../../environment/environment.dart';
+import '../../model/draft.dart';
+import '../main/main_service.dart';
 
 class DraftService extends MainService {
+
+  Logger logger = Logger();
+
+  static const String findMyDraftApi = "/private-app-api/find/my/draft";
+
+  Future<Draft> findMyDraft() async {
+    String api = "${Environment().apiUrl}$findMyDraftApi";
+
+    final SharedPreferences prefs = await getPrefs();
+
+    final response = await http.get(
+      Uri.parse(api),
+      headers: <String, String>{
+        "Content-Type": "application/json",
+        "Accept": "application/json; charset=UTF-8",
+        HttpHeaders.authorizationHeader: prefs.getString("token").toString()
+      },
+    );
+    if (response.statusCode == 200) {
+      return Draft.fromJson(jsonDecode(response.body));
+    }
+    else {
+      return throw Exception("Service 'findMyDraft' failed with statusCode: ${response.statusCode}");
+    }
+  }
 
 }
