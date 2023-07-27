534a8d806cb0bd3fb1a13c380fb12d9983d8461d
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-07-27 21:53:58 +0300

    Add addPostToDraft service method

--- lib/service/post_service/post_service.dart
+++ lib/service/post_service/post_service.dart
@@ -15,6 +15,7 @@ class PostService extends MainService {
   Logger logger = Logger();
 
   static const String addPostApi = "/private-app-api/add/post";
+  static const String addPostToDraftApi = "/private-app-api/add/post/to/draft";
 
   Future<bool> addPost(Post post) async {
     String api = "${Environment().apiUrl}$addPostApi";
@@ -35,6 +36,28 @@ class PostService extends MainService {
     }
     else {
       return throw Exception("Service 'addPost' failed with statusCode: ${response.statusCode}");
+    }
+  }
+
+  Future<bool> addPostToDraft(Post post) async {
+    String api = "${Environment().apiUrl}$addPostToDraftApi";
+
+    final SharedPreferences prefs = await getPrefs();
+
+    final response = await http.post(
+      Uri.parse(api),
+      headers: <String, String>{
+        "Content-Type": "application/json",
+        "Accept": "application/json; charset=UTF-8",
+        HttpHeaders.authorizationHeader: prefs.getString("token").toString()
+      },
+      body: jsonEncode(post.toJson())
+    );
+    if (response.statusCode == 200) {
+      return bool.tryParse(response.body) ?? false;
+    }
+    else {
+      return throw Exception("Service 'addPostToDraft' failed with statusCode: ${response.statusCode}");
     }
   }
 
