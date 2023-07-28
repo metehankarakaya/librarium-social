0a95fd2cd46c55f097eebc9227e2f9d0b30ab404
Metehan KARAKAYA <karakayametehan00@gmail.com>
2023-07-28 17:35:46 +0300

    Add deletePostInDraft method

--- lib/service/draft_service/draft_service.dart
+++ lib/service/draft_service/draft_service.dart
@@ -15,6 +15,7 @@ class DraftService extends MainService {
   Logger logger = Logger();
 
   static const String findMyDraftApi = "/private-app-api/find/my/draft";
+  static const String deletePostInDraftApi = "/private-app-api/delete/post/in/draft/";
 
   Future<Draft> findMyDraft() async {
     String api = "${Environment().apiUrl}$findMyDraftApi";
@@ -34,6 +35,27 @@ class DraftService extends MainService {
     }
     else {
       return throw Exception("Service 'findMyDraft' failed with statusCode: ${response.statusCode}");
+    }
+  }
+
+  Future<bool> deletePostInDraft(String tempId) async {
+    String api = "${Environment().apiUrl}$deletePostInDraftApi$tempId";
+
+    final SharedPreferences prefs = await getPrefs();
+
+    final response = await http.get(
+      Uri.parse(api),
+      headers: <String, String>{
+        "Content-Type": "application/json",
+        "Accept": "application/json; charset=UTF-8",
+        HttpHeaders.authorizationHeader: prefs.getString("token").toString()
+      },
+    );
+    if (response.statusCode == 200) {
+      return bool.tryParse(response.body) ?? false;
+    }
+    else {
+      return throw Exception("Service 'deletePostInDraft' failed with statusCode: ${response.statusCode}");
     }
   }
 
